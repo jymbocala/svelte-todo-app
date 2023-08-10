e7522c46f038da0b9ffec86e3629eb88ef30ca66
Jym Bocala <jym.bocala@gmail.com>
2023-08-10 17:51:19 +1000

    modify tailwind configuration  object

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-    theme: {
-    extend: {}
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./src/**/*.svelte', './src/**/*.css'],
+  darkMode: false,
+  theme: {
+    extend: {},
   },
-  plugins: []
-};
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
