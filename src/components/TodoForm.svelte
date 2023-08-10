d29e7af8238db30997bd62e2a2c3f64cce4046bf
Jym Bocala <jym.bocala@gmail.com>
2023-08-10 20:11:22 +1000

    add form tailwind styles

--- src/components/TodoForm.svelte
+++ src/components/TodoForm.svelte
@@ -1,7 +1,12 @@
-<form>
-  <div>
-    <label for="todo"></label>
-    <input type="text" name="todo" placeholder="Enter task" />
-  </div>
-  <button type="submit" class="">Add task</button>
+<form class="my-6">
+	<div class="flex flex-col text-sm mb-2">
+		<label for="todo" class="font-bold mb-2 text-gray-800">Todo</label>
+		<input
+			type="text"
+			name="todo"
+			placeholder="Enter task"
+			class="appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg"
+		/>
+	</div>
+	<button type="submit" class="w-full shadow-sm rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">Add task</button>
 </form>
