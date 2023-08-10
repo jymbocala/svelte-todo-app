ba7f0f636722f7958ebc8bdd3146fc722281cbe6
Jym Bocala <jym.bocala@gmail.com>
2023-08-10 20:18:52 +1000

    add functionality

--- src/components/TodoForm.svelte
+++ src/components/TodoForm.svelte
@@ -1,4 +1,13 @@
-<form class="my-6">
+<script>
+	let todo = '';
+	console.log(todo);
+
+	const handleSubmit = () => {
+		console.log('submitting');
+	};
+</script>
+
+<form class="my-6" on:submit|preventDefault={handleSubmit}>
 	<div class="flex flex-col text-sm mb-2">
 		<label for="todo" class="font-bold mb-2 text-gray-800">Todo</label>
 		<input
@@ -6,7 +15,12 @@
 			name="todo"
 			placeholder="Enter task"
 			class="appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg"
+			bind:value={todo}
 		/>
 	</div>
-	<button type="submit" class="w-full shadow-sm rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">Add task</button>
+	<button
+		type="submit"
+		class="w-full shadow-sm rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
+		>Add task</button
+	>
 </form>
