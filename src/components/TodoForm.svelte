2f023f987a50421b324db1bc8ca9b752ef22dd2a
Jym Bocala <jym.bocala@gmail.com>
2023-08-10 18:05:31 +1000

    create basic form component

---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,7 @@
+<form>
+  <div>
+    <label for="todo"></label>
+    <input type="text" name="todo" placeholder="Enter task" />
+  </div>
+  <button type="submit" class="">Add task</button>
+</form>
